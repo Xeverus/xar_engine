39804f9460889f00b7c55f7b845124f430ea3f87
Michal Wendelberger <michal.wendel@gmail.com>
2025-02-13 15:20:53 +0100

    test: enum_to_short for non-existing enum value

--- engine/test/unit/xar_engine/meta/enum_test.cpp
+++ engine/test/unit/xar_engine/meta/enum_test.cpp
@@ -36,18 +36,23 @@ ENUM_TO_STRING_IMPL(EnumTestEnum,
 
 namespace
 {
-    TEST(enum_in_namespace, enum_to_string)
+    TEST(enum_,
+         enum_in_namespace__enum_to_string)
     {
         const auto* const value_0 = xar_engine::meta::enum_to_string(enum_test::ns::EnumTestEnum::VALUE_0);
         const auto* const value_1 = xar_engine::meta::enum_to_string(enum_test::ns::EnumTestEnum::VALUE_1);
         const auto* const value_2 = xar_engine::meta::enum_to_string(enum_test::ns::EnumTestEnum::VALUE_2);
 
-        EXPECT_STREQ(value_0, "enum_test::ns::EnumTestEnum::VALUE_0");
-        EXPECT_STREQ(value_1, "enum_test::ns::EnumTestEnum::VALUE_1");
-        EXPECT_STREQ(value_2, "enum_test::ns::EnumTestEnum::VALUE_2");
+        EXPECT_STREQ(value_0,
+                     "enum_test::ns::EnumTestEnum::VALUE_0");
+        EXPECT_STREQ(value_1,
+                     "enum_test::ns::EnumTestEnum::VALUE_1");
+        EXPECT_STREQ(value_2,
+                     "enum_test::ns::EnumTestEnum::VALUE_2");
     }
 
-    TEST(enum_in_namespace, enum_to_string_operator)
+    TEST(enum_,
+         enum_in_namespace__enum_to_string_operator)
     {
         std::stringstream str_value_0;
         std::stringstream str_value_1;
@@ -57,34 +62,46 @@ namespace
         str_value_1 << enum_test::ns::EnumTestEnum::VALUE_1;
         str_value_2 << enum_test::ns::EnumTestEnum::VALUE_2;
 
-        EXPECT_STREQ(str_value_0.str().c_str(), "enum_test::ns::EnumTestEnum::VALUE_0");
-        EXPECT_STREQ(str_value_1.str().c_str(), "enum_test::ns::EnumTestEnum::VALUE_1");
-        EXPECT_STREQ(str_value_2.str().c_str(), "enum_test::ns::EnumTestEnum::VALUE_2");
+        EXPECT_STREQ(str_value_0.str().c_str(),
+                     "enum_test::ns::EnumTestEnum::VALUE_0");
+        EXPECT_STREQ(str_value_1.str().c_str(),
+                     "enum_test::ns::EnumTestEnum::VALUE_1");
+        EXPECT_STREQ(str_value_2.str().c_str(),
+                     "enum_test::ns::EnumTestEnum::VALUE_2");
     }
 
-    TEST(enum_in_namespace, enum_to_short_string)
+    TEST(enum_,
+         enum_in_namespace__enum_to_short_string)
     {
         const auto* const value_0 = xar_engine::meta::enum_to_short_string(enum_test::ns::EnumTestEnum::VALUE_0);
         const auto* const value_1 = xar_engine::meta::enum_to_short_string(enum_test::ns::EnumTestEnum::VALUE_1);
         const auto* const value_2 = xar_engine::meta::enum_to_short_string(enum_test::ns::EnumTestEnum::VALUE_2);
 
-        EXPECT_STREQ(value_0, "VALUE_0");
-        EXPECT_STREQ(value_1, "VALUE_1");
-        EXPECT_STREQ(value_2, "VALUE_2");
+        EXPECT_STREQ(value_0,
+                     "VALUE_0");
+        EXPECT_STREQ(value_1,
+                     "VALUE_1");
+        EXPECT_STREQ(value_2,
+                     "VALUE_2");
     }
 
-    TEST(enum_without_namespace, enum_to_string)
+    TEST(enum_,
+         enum_without_namespace__enum_to_string)
     {
         const auto* const value_0 = xar_engine::meta::enum_to_string(EnumTestEnum::VALUE_10);
         const auto* const value_1 = xar_engine::meta::enum_to_string(EnumTestEnum::VALUE_11);
         const auto* const value_2 = xar_engine::meta::enum_to_string(EnumTestEnum::VALUE_12);
 
-        EXPECT_STREQ(value_0, "EnumTestEnum::VALUE_10");
-        EXPECT_STREQ(value_1, "EnumTestEnum::VALUE_11");
-        EXPECT_STREQ(value_2, "EnumTestEnum::VALUE_12");
+        EXPECT_STREQ(value_0,
+                     "EnumTestEnum::VALUE_10");
+        EXPECT_STREQ(value_1,
+                     "EnumTestEnum::VALUE_11");
+        EXPECT_STREQ(value_2,
+                     "EnumTestEnum::VALUE_12");
     }
 
-    TEST(enum_without_namespace, enum_to_string_operator)
+    TEST(enum_,
+         enum_without_namespace__enum_to_string_operator)
     {
         std::stringstream str_value_0;
         std::stringstream str_value_1;
@@ -94,19 +111,38 @@ namespace
         str_value_1 << EnumTestEnum::VALUE_11;
         str_value_2 << EnumTestEnum::VALUE_12;
 
-        EXPECT_STREQ(str_value_0.str().c_str(), "EnumTestEnum::VALUE_10");
-        EXPECT_STREQ(str_value_1.str().c_str(), "EnumTestEnum::VALUE_11");
-        EXPECT_STREQ(str_value_2.str().c_str(), "EnumTestEnum::VALUE_12");
+        EXPECT_STREQ(str_value_0.str().c_str(),
+                     "EnumTestEnum::VALUE_10");
+        EXPECT_STREQ(str_value_1.str().c_str(),
+                     "EnumTestEnum::VALUE_11");
+        EXPECT_STREQ(str_value_2.str().c_str(),
+                     "EnumTestEnum::VALUE_12");
     }
 
-    TEST(enum_without_namespace, enum_to_short_string)
+    TEST(enum_,
+         enum_without_namespace__enum_to_short_string)
     {
         const auto* const value_0 = xar_engine::meta::enum_to_short_string(EnumTestEnum::VALUE_10);
         const auto* const value_1 = xar_engine::meta::enum_to_short_string(EnumTestEnum::VALUE_11);
         const auto* const value_2 = xar_engine::meta::enum_to_short_string(EnumTestEnum::VALUE_12);
 
-        EXPECT_STREQ(value_0, "VALUE_10");
-        EXPECT_STREQ(value_1, "VALUE_11");
-        EXPECT_STREQ(value_2, "VALUE_12");
+        EXPECT_STREQ(value_0,
+                     "VALUE_10");
+        EXPECT_STREQ(value_1,
+                     "VALUE_11");
+        EXPECT_STREQ(value_2,
+                     "VALUE_12");
+    }
+
+    TEST(enum_,
+         non_existing_value__should_throw)
+    {
+        const auto non_existing_value = static_cast<enum_test::ns::EnumTestEnum>(0xFFFF);
+        EXPECT_THROW(
+            []()
+            {
+                return xar_engine::meta::enum_to_string(non_existing_value);
+            }(),
+            xar_engine::error::XarException);
     }
 }
