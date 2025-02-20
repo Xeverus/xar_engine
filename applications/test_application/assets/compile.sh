66c7514ba3d77e444572ae5251d98aa1131a5ff5
Michal Wendelberger <michal.wendel@gmail.com>
2025-02-20 18:08:27 +0100

    chore: remove old resources

--- applications/test_application/assets/compile.sh
+++ applications/test_application/assets/compile.sh
@@ -1,5 +1,2 @@
-glslc.exe hardcoded_triangle.vert -o hardcoded_triangle.vert.spv
-glslc.exe hardcoded_triangle.frag -o hardcoded_triangle.frag.spv
-
 glslc.exe triangle.vert -o triangle.vert.spv
 glslc.exe triangle.frag -o triangle.frag.spv
